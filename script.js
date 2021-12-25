e0abac27750a9505bf8cc21a4381966c5a991c0a
Speed Coders <95556455+speed-coders@users.noreply.github.com>
2021-12-25 15:41:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById('nokey'),
    can_h = parseInt(canvas.getAttribute('height')),
    ctx = canvas.getContext('2d');
 
-// console.log(typeof can_w);
+console.log(typeof can_w);
 var BALL_NUM = 30
 
 var ball = {
